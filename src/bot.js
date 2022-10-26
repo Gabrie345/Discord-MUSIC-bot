24895bcad310192d3282ad3dd65466fa54bb7ae7
Gabrie345 <gabriel345wp@gmail.com>
2022-10-26 12:45:42 -0300

    Correção falha de segurança Token

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,4 @@
-const { token } = process.env;
+
 const Discord = require("discord.js");
 const { GatewayIntentBits } = require("discord.js");
 const dotenv = require("dotenv");
@@ -8,10 +8,10 @@ dotenv.config();
 const client = new Discord.Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
-
+const { token } = process.env;
 const prefix = "#";
 
-client.login("MTAzNDE2MTM2NDQ1OTA2NTM5NA.GUuaVs.vDMx-_6IIuPma7rKq7Ip0_MPReoKnQG9R8NaDo");
+client.login(token);
 
 client.on("ready", () => {
   console.log("Bot is ready!!!");
@@ -30,13 +30,5 @@ client.on("messageCreate", (msg) => {
       content: "SIM!!!!!!",
     });
   }
-  if(msg.content === '#join'){
-    if(msg.member.voiceChannel){
-        msg.member.voiceChannel.join()
-        msg.channel.send('O Pai ta on');
-        console.log('o pai ta ON !!!');
-    }else{
-        msg.channel.send('Burro você não ta no canal de voz.....');
-        console.log('Burro você não ta no canal de voz.....');
-    }
+  
 });
